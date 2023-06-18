ea2c4b894737a9825e5d9f89f0063704e603329b
dducws <ductiennguyen@pm.me>
2023-06-18 20:57:06 +0700

    Create CSES folder and upload the solution for a problem named apple_divison

---
+++ platforms/CSES/apply_division.cpp
@@ -0,0 +1,42 @@
+// Apple Division - CSES Problem Set 
+// Problem: https://cses.fi/problemset/task/1623
+// Solution: https://usaco.guide/bronze/complete-rec?lang=cpp#problems
+// Edited: chatGPT
+
+
+#include <iostream> 
+#include <vector>
+
+using ll = long long;
+
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+ll findMinDifference(int index, ll sumGroup1, ll sumGroup2, const std::vector<ll>& appleWeights)
+{
+    if (index == sz(appleWeights))
+        return (std::abs(sumGroup1 - sumGroup2));
+    
+    return std::min(findMinDifference(index + 1, sumGroup1 + appleWeights[index], sumGroup2, appleWeights),
+                    findMinDifference(index + 1, sumGroup1, sumGroup2 + appleWeights[index], appleWeights));
+}
+
+void solve()
+{
+    int n;
+    std::cin >> n;
+    std::vector<ll> appleWeights (n);
+    for (int i {}; i < n; ++i)
+    {
+        std::cin >> appleWeights[i];
+    }
+
+    ll minDifference {findMinDifference(0, 0, 0, appleWeights)};
+    std::cout << minDifference;
+}
+
+int main()
+{
+    std::cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
