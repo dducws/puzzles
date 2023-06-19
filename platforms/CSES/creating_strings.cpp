f21e44b28d398a5338d15edf5863fe8fee38e3a5
dducws <ductiennguyen@pm.me>
2023-06-19 12:15:09 +0700

    Create creating_strings.cpp

---
+++ platforms/CSES/creating_strings.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <vector>
+#include <algorithm>
+
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+
+void solve()
+{
+    std::string s;
+    std::cin >> s; 
+    std::sort(all(s));
+    std::vector<std::string> permutations; 
+
+    do 
+    {
+        permutations.push_back(s);
+    }
+    while (std::next_permutation(all(s)));
+
+    std::cout << sz(permutations) << '\n';
+    for (const auto& permutation : permutations)
+    {
+        std::cout << permutation << '\n';
+    }
+
+}
+
+int main()
+{
+    std::cin.tie(0)->sync_with_stdio(0);
+    solve();
+}
